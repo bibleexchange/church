37ca9b094f6997197202f32c646f35b6f5c5b9b8
Stephen Reynolds <sgrjr@deliverance.me>
2020-04-04 02:56:03 -0400

    fixed

--- app/Http/Controllers/ChurchController.php
+++ app/Http/Controllers/ChurchController.php
@@ -8,48 +8,18 @@ class ChurchController extends Controller
 {
     public function index(Request $request)
     {
-        $data = new \App\Sermon();
-        return view('welcome', compact('data') );
+       $sermons = \App\Sermon::prepare();
+        return view('welcome', compact('sermons') );
     }
 
     public function sermons(Request $request)
     {
-
-        $sermons = [];
-
-        $data = new \App\Sermon();
-
-        $i=0;
-        foreach($data->data->Contents AS $c){
-
-            $s = new \stdclass;
-            $s->Key = (String) $c->Key;
-
-            $id = explode("/", $c->Key);
-
-            if(count($id) === 2 && $id[1] !== ""){
-           
-                $id = explode('.',$id[1]);
-                $rec = \App\Recording::where('date','LIKE','%'.$id[0].'%')->first();
-
-                if(is_object($rec)){
-                    $s->Title = $rec->title;
-                }else{
-                    $s->Title= $id;
-                }
-                
-            }else{
-                 $s->Title = $id[0];
-            }
-            $sermons[$i] = $s;
-            $i++;
-        }
-       
+        $sermons = \App\Sermon::prepare();
         return view('sermons', compact('sermons'));
     }
     public function live(Request $request)
     {
-        $data = new \App\Sermon();
-        return view('live', compact('data'));
+        $sermons = \App\Sermon::prepare();
+        return view('live', compact('sermons'));
     }
 }
--- app/Sermon.php
+++ app/Sermon.php
@@ -39,4 +39,38 @@ class Sermon
         return json_decode($data,TRUE);
     }
 
+    public static function prepare(){
+         $sermons = [];
+
+        $data = new static();
+
+        $i=0;
+        foreach($data->data->Contents AS $c){
+
+            $s = new \stdclass;
+            $s->Key = (String) $c->Key;
+
+            $id = explode("/", $c->Key);
+
+            if(count($id) === 2 && $id[1] !== ""){
+           
+                $id = explode('.',$id[1]);
+                $rec = \App\Recording::where('date','LIKE','%'.$id[0].'%')->first();
+
+                if(is_object($rec)){
+                    $s->Title = $rec->title;
+                }else{
+                    $s->Title= $id;
+                }
+                
+            }else{
+                 $s->Title = $id[0];
+            }
+            $sermons[$i] = $s;
+            $i++;
+        }
+
+        return $sermons;
+    }
+
 }
